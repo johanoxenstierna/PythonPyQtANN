0b3ee55ac85b640cae604cd993ea4299cea5981b
johanoxenstierna <joox9891@gmail.com>
2017-03-30 11:16:12 +0200

    i

---
+++ 2SettingUpBeforeBProp/ANN/TrainingData.py
@@ -0,0 +1,33 @@
+
+
+class TrainingData:
+    def __init__(self):
+        super().__init__()
+        self.text_file = open("myTrainingDataFile.txt")
+
+    def get_next_inputs(self):
+        next_line = self.text_file.readline()
+        next_line_list = next_line.split(";")
+        return [next_line_list[1], next_line_list[2], next_line_list[4]]
+
+    def end_file_read(self):
+        self.text_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
